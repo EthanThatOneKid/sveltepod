e14ef968ca4c764da8c80ab85c4bf33c2db85451
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-04-29 01:36:05 -0700

    Add example 00

---
+++ examples/00/sample_counter.svelte
@@ -0,0 +1,12 @@
+<script>
+	let count = 0;
+</script>
+
+<input type=number bind:value={count}>
+<button on:click="{() => count += 1}">count</button>
+
+<style>
+  input {
+    outline: 5px solid lime;
+  }
+</style>
