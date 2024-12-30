183171a204fca1a3b94d8cc8cde9493352346096
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-12-29 19:53:28 -0800

    add svelte2tsx samples
    Source: https://github.com/sveltejs/language-tools/tree/ac10174d3ccda134d0d43d3191e2ce8acfe91c30/packages/svelte2tsx/test/svelte2tsx/samples

---
+++ svelte2tsx-samples/ts-$$generics-dts/input.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    type A = $$Generic;
+    type B = $$Generic<keyof A>;
+    type C = $$Generic<boolean>;
+
+    export let a: A;
+    export let b: B;
+    export let c: C;
+
+    const dispatch = createEventDispatcher<{a: A}>();
+
+    export function getA() {
+        return a;
+    }
+</script>
+
+<slot {c} />
